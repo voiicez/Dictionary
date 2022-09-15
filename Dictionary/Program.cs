888e77b8a897ffba6271e2b062d79436a6087208
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-09-15 13:34:15 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> sozluk = new Dictionary<string, string>();
+            sozluk.Add("table", "tablo");
+            sozluk.Add("book", "kitap");
+            Console.WriteLine(sozluk["table"]);// tablo
+            foreach (var eleman in sozluk)
+            {
+                Console.WriteLine(eleman);
+            }
+            foreach (var eleman in sozluk)
+            {
+                Console.WriteLine(eleman.Key+" - "+eleman.Value);
+            }
+
+            try
+            {
+                string[] ogrenciler = new string[2];
+                Console.WriteLine(ogrenciler[2]); // Burada try olmasaydı uygulama kapanacaktı.
+            }
+            catch
+            {
+                Console.WriteLine("HATA MEYDANA GELDİ!");
+            }
+            try
+            {
+                string ad = null;
+                ad = ad.Trim(); //Null reference exception verecek normalde.
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Hata adı:"+ ex.Message);
+            }
+            try
+            {
+                string path = @"C:\abidik\gubidik.txt";
+                string icerik = File.ReadAllText(path);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
